4fde0018e3803db90428e52b37e206cdeee3eae6
jaffrantirta <franartika@gmail.com>
2022-06-13 14:49:09 +0800

    integrated tailwind

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -8,6 +8,7 @@
 
         <!-- Fonts -->
         <link href="https://fonts.googleapis.com/css2?family=Nunito:wght@400;600;700&display=swap" rel="stylesheet">
+        <link href="/css/app.css" rel="stylesheet">
 
         <!-- Styles -->
         <style>
@@ -128,5 +129,6 @@
                 </div>
             </div>
         </div>
+        <script src="../path/to/flowbite/dist/flowbite.js"></script>
     </body>
 </html>
