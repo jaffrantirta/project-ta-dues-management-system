e9592a3f86f2aee6cb891451d5302e8f5b2fdef7
jaffrantirta <franartika@gmail.com>
2022-07-09 21:50:20 +0800

    error handling

--- app/Http/Controllers/LandingController.php
+++ app/Http/Controllers/LandingController.php
@@ -10,14 +10,14 @@ class LandingController extends Controller
 {
     public function index()
     {
-        $data['title1'] = Setting::where('key', 'title1')->first()->content;
-        $data['subtitle1'] = Setting::where('key', 'subtitle1')->first()->content;
-        $data['quotes'] = Setting::where('key', 'quotes')->first()->content;
+        (Setting::where('key', 'title1')->exists()) ? $data['title1'] = Setting::where('key', 'title1')->first()->content : $data['title1'] = 'Title 1';
+        (Setting::where('key', 'title2')->exists()) ? $data['title2'] = Setting::where('key', 'title2')->first()->content : $data['title2'] = 'Title 2';
+        (Setting::where('key', 'subtitle1')->exists()) ? $data['subtitle1'] = Setting::where('key', 'subtitle1')->first()->content : $data['subtitle1'] = 'Subtitle 1';
+        (Setting::where('key', 'subtitle2')->exists()) ? $data['subtitle2'] = Setting::where('key', 'subtitle2')->first()->content : $data['subtitle2'] = 'Subtitle 2';
+        (Setting::where('key', 'picture1')->exists()) ? $data['picture1'] = Setting::where('key', 'picture1')->first()->content : $data['picture1'] = 'Picture 1';
+        (Setting::where('key', 'picture2')->exists()) ? $data['picture2'] = Setting::where('key', 'picture2')->first()->content : $data['picture2'] = 'Picture 2';
+        (Setting::where('key', 'quotes')->exists()) ? $data['quotes'] = Setting::where('key', 'quotes')->first()->content : $data['quotes'] = 'Quotes';
         $data['events'] = Event::whereDate('date_time', '>=', date('Y-m-d'))->limit(4)->orderBy('date_time', 'asc')->get();
-        $data['title2'] = Setting::where('key', 'title2')->first()->content;
-        $data['subtitle2'] = Setting::where('key', 'subtitle2')->first()->content;
-        $data['picture1'] = Setting::where('key', 'picture1')->first()->content;
-        $data['picture2'] = Setting::where('key', 'picture2')->first()->content;
         // return $data;
         return view('landing.index', $data);
     }
